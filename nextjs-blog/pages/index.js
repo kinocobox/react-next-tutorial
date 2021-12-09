c340b7547f6707e431de6ca1a9dbfa40d3911250
h.kinoshita <hiroki.kinoshita@kuno-corp.com>
2021-12-09 12:57:08 +0900

    feat: pages/index に md の記事情報を出力する処理を追加

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -2,18 +2,35 @@ import Head from 'next/head'
 import utilStyles from '../styles/utils.module.css'
 import Layout, { siteTitle }  from '../components/layout'
 
-export default function Home() {
+import { getSortedPostData } from '../lib/post'
+export async function getStaticProps() {
+  const allPostsData = getSortedPostData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (this is a sample website - you'll be building a site like this on {' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
