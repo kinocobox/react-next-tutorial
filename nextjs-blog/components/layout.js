a15da2474792e89757ec319ffee8f49aa492983d
h.kinoshita <hiroki.kinoshita@kuno-corp.com>
2021-12-09 10:38:57 +0900

    feat: component/layout の修正

--- nextjs-blog/components/layout.js
+++ nextjs-blog/components/layout.js
@@ -1,4 +1,57 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
 import styles from './layout.module.css'
-export default function Layout({ children }) {
-    return <div className={styles.container}>{children}</div>
+import utilStyles from '../styles/utils.module.css'
+
+const name ='Your Name'
+export const siteTitle = 'Next.js Sample Website'
+
+export default function Layout({ children, home }) {
+    return (
+        <div className={styles.container}>
+            <Head>
+                <link rel="icon" href="/favicon.ico" />
+                <meta
+                    name="description"
+                    content="Learn how to build a personal website using Next.js"
+                />"
+                <meta name="og:title" content={siteTitle} />
+                <meta name="twitter:card" content="summary_large_image" />
+            </Head>
+            <header className={styles.header}>
+                {home ? (
+                    <>
+                        <Image
+                            priority
+                            src="/image/profile.jpg"
+                            className={utilStyles.borderCicle}
+                            height={144}
+                            width={144}
+                            alt={name}
+                        >
+                        </Image>
+                        <h1 className={utilStyles.heading2Xl}>{name}</h1>
+                    </>
+                ) : (
+                    <>
+                        <h2 className={utilStyles.headingLg}>
+                            <Link href="/">
+                                <a className={utilStyles.colorInherit}>{name}</a>
+                            </Link>
+                        </h2>
+                    </>
+                    )
+                }
+            </header>
+            <main>{children}</main>
+            {!home && (
+                <div className={styles.backToHome}>
+                    <Link href="/">
+                        <a>⇦Back to home</a>
+                    </Link>
+                </div>
+            )}
+        </div>
+    )
 }
